4509ee0f8faae625ea0a01b4417de387778bfeb8
RocioAV <44351449@fi.unju.edu.ar>
2025-07-03 22:40:24 -0300

    se agregaron funcionalidades a paciente y turno

--- src/app/services/paciente.service.ts
+++ src/app/services/paciente.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
+import { Paciente } from './turno.service';
 
 @Injectable({
   providedIn: 'root'
@@ -16,5 +17,12 @@ export class PacienteService {
 
   getPacienteById(id: string) {
     return this.http.get(`${this.apiUrl}/${id}`);
+  }
+  // Angular service
+  getPacienteByDni(dni: string) {
+    return this.http.get(`${this.apiUrl}/dni/${dni}`);
+  }
+  getAllPacientes() {
+    return this.http.get<Paciente[]>(`${this.apiUrl}`);
   }
 }
