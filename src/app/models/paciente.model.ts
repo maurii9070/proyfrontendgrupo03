04af60390922cdcf3288d7ee0e0de85827b8c53a
facugsXXI <102118757+facugsXXI@users.noreply.github.com>
2025-07-04 00:58:58 -0300

    Actualizaciones en los modelos y en los servicios de pacientes, doctores y especialidades

---
+++ src/app/models/paciente.model.ts
@@ -0,0 +1,10 @@
+export interface Paciente {
+  _id: string;
+  nombre: string;
+  apellido: string;
+  dni: string;
+  telefono: string;
+  fechaNacimiento: string;
+  email: string;
+  password: string;
+} 
